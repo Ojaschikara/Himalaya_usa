567f33e68e48f1d8de4aa02b6c4ea8be094fb5ed
Ojaschikara <ojaschikara@email.com>
2024-08-13 21:09:33 +0530

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import AllRoutes from "./components/AllRoutes";
 import Navbar from "./components/Navbar";
-
+import DragAndDrop from "./pages/dragAndDrop"
 function App() {
   return (
     <>
       <Navbar />
+      
       <AllRoutes />
     </>
   );
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,73 +1,73 @@
-import {
-  Box,
-  Heading,
-  Input,
-  Button,
-  VStack,
-  Container,
-} from "@chakra-ui/react";
-import { useState, useContext } from "react";
-import axios from "axios";
-import { AuthContext } from "../context/AuthContextProvider";
-import { Navigate } from "react-router-dom";
+// import {
+//   Box,
+//   Heading,
+//   Input,
+//   Button,
+//   VStack,
+//   Container,
+// } from "@chakra-ui/react";
+// import { useState, useContext } from "react";
+// import axios from "axios";
+// import { AuthContext } from "../context/AuthContextProvider";
+// import { Navigate } from "react-router-dom";
 
-export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+// export default function Login() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
 
-  const {
-    login,
-    authDetails: { isLoggedIn },
-  } = useContext(AuthContext);
+//   const {
+//     login,
+//     authDetails: { isLoggedIn },
+//   } = useContext(AuthContext);
 
-  async function handleClick() {
-    try {
-      let resp = await axios({
-        method: "post",
-        url: "https://reqres.in/api/login",
-        data: {
-          email,
-          password,
-        },
-      });
+//   async function handleClick() {
+//     try {
+//       let resp = await axios({
+//         method: "post",
+//         url: "https://reqres.in/api/login",
+//         data: {
+//           email,
+//           password,
+//         },
+//       });
 
-      login(resp?.data?.token);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+//       login(resp?.data?.token);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
 
-  if (isLoggedIn) {
-    return <Navigate to="/" />;
-  }
+//   if (isLoggedIn) {
+//     return <Navigate to="/" />;
+//   }
 
-  return (
-    <Container maxW={"600px"}>
-      <VStack spacing={6}>
-        <Heading as="h1" size="xl">
-          Login Page
-        </Heading>
+//   return (
+//     <Container maxW={"600px"}>
+//       <VStack spacing={6}>
+//         <Heading as="h1" size="xl">
+//           Login Page
+//         </Heading>
 
-        <Input
-          placeholder="Email"
-          type="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <Input
-          placeholder="Password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
+//         <Input
+//           placeholder="Email"
+//           type="email"
+//           value={email}
+//           onChange={(e) => {
+//             setEmail(e.target.value);
+//           }}
+//         />
+//         <Input
+//           placeholder="Password"
+//           value={password}
+//           onChange={(e) => {
+//             setPassword(e.target.value);
+//           }}
+//         />
 
-        <Button variant="outline" colorScheme="red" onClick={handleClick}>
-          LOGIN
-        </Button>
-      </VStack>
-    </Container>
-  );
-}
+//         <Button variant="outline" colorScheme="red" onClick={handleClick}>
+//           LOGIN
+//         </Button>
+//       </VStack>
+//     </Container>
+//   );
+// }
