2bc8002dc6f864ace9e07a6891b619c1c546b0f3
Ojaschikara <ojaschikara@email.com>
2024-09-18 02:38:22 +0530

    added

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,23 +1,23 @@
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
+// const jwt = require('jsonwebtoken')
+// require('dotenv').config()
 
 
-const userAuth = async (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+// const userAuth = async (req, res, next) => {
+//   const token = req.headers.authorization?.split(" ")[1];
 
-  if (!token) {
-    return res.status(401).send({ message: "Token not provided" });
-  }
+//   if (!token) {
+//     return res.status(401).send({ message: "Token not provided" });
+//   }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.body.userID = decoded.userID;
+//     req.body.userID = decoded.userID;
 
-    next();
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
-};
+//     next();
+//   } catch (error) {
+//     res.status(500).send({ message: error.message });
+//   }
+// };
 
-module.exports = userAuth;
+// module.exports = userAuth;
