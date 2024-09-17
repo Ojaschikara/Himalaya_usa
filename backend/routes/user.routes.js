2991a7a47ffeee4bd5d61dc43e82b8e17bac1916
Ojaschikara <ojaschikara@email.com>
2024-09-18 02:37:55 +0530

    added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const userRegistration = require("../controllers/UserReq/userRegistration.controller");
-const userLogin = require("../controllers/UserReq/userLogin.controller");
-const userRouter = express.Router();
+// const express = require("express");
+// const userRegistration = require("../controllers/UserReq/userRegistration.controller");
+// const userLogin = require("../controllers/UserReq/userLogin.controller");
+// const userRouter = express.Router();
 
-userRouter.post("/register", userRegistration);
-userRouter.post("/login", userLogin);
+// userRouter.post("/register", userRegistration);
+// userRouter.post("/login", userLogin);
 
-module.exports = userRouter;
+// module.exports = userRouter;
