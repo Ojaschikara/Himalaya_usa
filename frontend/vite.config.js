20bbe656d6fef79306f3a9205dd1073e74d2ec7e
Ojaschikara <ojaschikara@email.com>
2024-09-20 18:57:54 +0530

    added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@redux': resolve(__dirname, 'src/redux'), // Create an alias for the redux folder
-    },
-  },
-});
+})
+
